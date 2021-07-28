a77e252e84acdece5ff4eb5b50f589fd262f5101
bilalmnfl <bilalm.nfl@gmail.com>
2021-07-28 17:01:44 +0530

    make it better

--- app.py
+++ app.py
@@ -10,22 +10,33 @@ def home():
    return "<center><h1>Welcome Pinterest API </h1></center>"
 
 # Login function
-
-
-@app.route('/login', methods=['POST'])
-
+@app.route('/login', methods=['GET'])
 def login():
-    data = request.get_json()
-    email = data["email"]
-    password = data["password"]
-    username = data["username"]
-    pinterest = Pinterest(email=email, password=password,
-                          username=username, cred_root='cred_root')
-    if email != '' and password != '' and username != '':
-        pinterest.login()
-        return jsonify({"message": "Thankyou for login", "status": 200})
+    pinterest = Pinterest(email='bilalmalik1561@gmail.com',
+                          password='bilal12345',
+                          username='bilalmalik1561',
+                          cred_root='cred_root')
+    pinterest.login()
+    return jsonify({"message": "Thankyou for login", "status": 200})
+ 
+
+# Get border ID function
+@app.route('/get_border_id', methods=['GET'])
+def getBoardIds():
+    pinterest = Pinterest(email='bilalmalik1561@gmail.com',
+                          password='bilal12345',
+                          username='bilalmalik1561',
+                          cred_root='cred_root')
+    username = "bilalmalik1561"
+
+    if username != '':
+            dictBoards = {}
+            boards = pinterest.boards(username=username)
+            for board in boards:
+                dictBoards[board['name']] = board['id']
+            return jsonify({"data": dictBoards, "status": 200})
     else:
-        return jsonify({"message": "Please fill input", "status": 400})
+        return jsonify({"message": "Please fill username", "status": 400})
 
 if __name__ == '__main__':
    app.run(debug=True)
