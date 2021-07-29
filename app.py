479884d71732d7740baf8335ca1b5c6b7b80d82e
bilalmnfl <bilalm.nfl@gmail.com>
2021-07-29 15:25:19 +0530

    make it better

--- app.py
+++ app.py
@@ -20,6 +20,14 @@ def home():
 # Login function
 @app.route('/login', methods=['GET'])
 def login():
+    request_data = request.get_json()
+    username = request_data['username']
+    email = request_data['email']
+    password = request_data['password']
+    pinterest = Pinterest(email=email,
+                          password=password,
+                          username=username,
+                          cred_root='cred_root')
     pinterest.login()
     return jsonify({"message": "Thankyou for login", "status": 200})
 
@@ -27,6 +35,8 @@ def login():
 # Get border ID function
 @app.route('/get_border_id', methods=['GET'])
 def getBoardIds():
+    request_data = request.get_json()
+    username = request_data['username']
     dictBoards = {}
     boards = pinterest.boards(username=username)
     for board in boards:
@@ -36,6 +46,8 @@ def getBoardIds():
 # get_user_profile function
 @app.route('/get_user_profile', methods=['GET'])
 def get_user_profile():
+    request_data = request.get_json()
+    username = request_data['username']
     return pinterest.get_user_overview(username=username)
 
 # create_board function
@@ -43,7 +55,7 @@ def get_user_profile():
 def create_board():
     request_data = request.get_json()
     name = request_data['name'],
-    description = request_data['description'],
+    description = request_data['description']
     category = 'other',
     privacy = 'public',
     layout = 'default'
@@ -58,8 +70,8 @@ def create_board():
 @app.route('/create_board_section', methods=['GET'])
 def create_board_section():
     request_data = request.get_json()
-    board_id = request_data['board_id'],
-    section_name = request_data['section_name'],
+    board_id = request_data['board_id']
+    section_name = request_data['section_name']
 
     if board_id != '' and section_name != '':
         pinterest.create_board_section(
@@ -72,7 +84,7 @@ def create_board_section():
 @app.route('/get_board_sections', methods=['GET'])
 def get_board_sections():
     request_data = request.get_json()
-    board_id = request_data['board_id'],
+    board_id = request_data['board_id']
     if board_id != '':
         data = pinterest.get_board_sections(board_id=board_id)
         return jsonify({"message": data, "status": 200})
@@ -83,12 +95,29 @@ def get_board_sections():
 @app.route('/delete_board_section', methods=['GET'])
 def delete_board_section():
     request_data = request.get_json()
-    section_id = request_data['section_id'],
+    section_id = request_data['section_id']
     if section_id != '':
         pinterest.delete_board_section(section_id=section_id)
         return jsonify({"message": "Section Deleted!", "status": 200})
     else:
         return jsonify({"message": "Please fill section_id", "status": 404})
+
+
+@app.route('/delete_board_section', methods=['POST'])
+def pin():
+    request_data = request.get_json()
+    board_id = request_data['board_id']
+    section_id = None
+    image_url = request_data['image_url']
+    description = request_data['description']
+    title = request_data['title']
+    link = request_data['link']
+    if section_id != '':
+        pinterest.pin(board_id=board_id, section_id=section_id, image_url=image_url, description=description,
+                      title=title, link=link)
+        return jsonify({"message": "Section Deleted!", "status": 200})
+    else:
+        return jsonify({"message": "Please fill all input", "status": 404})
 
 if __name__ == '__main__':
    app.run(debug=True)
